485257e0d06a5bd31604530c7813bd229a79f6b8
STL CAPS <stlcaps@AFTN.local>
2021-12-01 12:59:15 -0600

    learned to declare variables and its types

--- main.js
+++ main.js
@@ -1,15 +1,58 @@
-console.log("log");
+// console.log("log");
 
-document.querySelectorAll(".btn").forEach((item) => {
-  item.addEventListener("click", () => {
-    alert("Hello CAPS");
-  });
-});
+// document.querySelectorAll(".btn").forEach((item) => {
+//   item.addEventListener("click", () => {
+//     alert("Hello CAPS");
+//   });
+// });
 
-// helper methods
+// // helper methods
 
-alert({ name: "HiHi Hani", age: 17, email: "hanah@hanah.com", active: true });
+// alert({ name: "HiHi Hani", age: 17, email: "hanah@hanah.com", active: true });
 
-document.write({ name: "HiHi Hani", email: "hanah@hanah.com" });
+// document.write({ name: "HiHi Hani", email: "hanah@hanah.com" });
 
-console.log({ name: "HiHi Hani", email: "hanah@hanah.com" });
+// console.log({ name: "HiHi Hani", email: "hanah@hanah.com" });
+
+// Declare Variable
+
+let age1 = 17;
+// const age = 12;
+
+console.log(age1);
+
+// let lastName;
+// lastName = "Sakinovic";
+
+const firstName = "Hanah";
+const lastName = "Sakinovic";
+
+let fullName;
+
+fullName = firstName + " " + lastName;
+
+console.log("Hey! My full name is " + fullName + " :3");
+
+// console.log(lastName);
+
+const finn = "Adventure Time c'mon grab your friends!";
+
+console.log(finn);
+
+const website = "google";
+
+const url = "http://www." + website + ".com";
+
+console.log(url);
+
+// let a = 5;
+// let b = 10;
+
+// age = 34;
+
+// console.log(`Fifteen is ${age}`);
+
+let name = "hanah"; //string
+let age = 24; //number
+let age = "46"; //STRING, not a #; REMEMBER strings are BLACK and #s are BLUE
+let userProfile = true; //boolean
