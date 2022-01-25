1e32e5105802710ff282ff5725421547926e4f18
STL CAPS <stlcaps@AFTN.local>
2022-01-25 11:44:07 -0600

    finished 10th hour code

--- 10th hour/main.js
+++ 10th hour/main.js
@@ -53,3 +53,87 @@ betterItems.forEach(function (i) {
   // anonymous function
   console.log(i);
 });
+
+//getElementsByClassName('className'); // multiple elements
+// node-list = array-like
+// index, length, but not array methods
+
+// const fruits = document.getElementsByClassName("special");
+// console.log(fruits);
+// fruits[0].style.color = "red";
+
+// querySelector('any css') // select single (first element)
+// querySelectorAll('any css') // selects all and is able to use methods (forEach)
+// must use . or # in for class or id, otherwise use tags
+
+// const fruits = document.querySelector(".special");
+// console.log(fruits);
+// fruits.style.color = "red";
+
+// const lastChild = document.querySelector("li:last-child");
+// lastChild.style.color = "lightpink";
+
+// const list = document.querySelectorAll(".special");
+// console.log(list);
+
+// list.forEach(function (i) {
+//   console.log(i);
+//   i.style.color = "blue";
+// });
+
+//childNodes - returns all childNodes including whitespace which is treated as text
+
+// const fruits = document.querySelector("#fruits");
+// console.log(fruits);
+
+// // children
+// const allChildren = fruits.children;
+// console.log(allChildren);
+
+// parent element
+
+// const heading2 = document.querySelector("h2");
+// console.log(heading2.parentElement);
+
+// const parent = heading2.parentElement;
+// console.log(parent);
+// parent.style.backgroundColor = "orange";
+
+// previousSibling
+// nextSibling
+// return whitespace - so type it twice to target the first
+
+// const first = document.querySelector(".first");
+// first.style.color = "red";
+
+// const second = first.nextSibling.nextSibling.nextSibling.nextSibling;
+// second.style.color = "orange";
+// console.log(second);
+
+// getAttribute
+// setAttribute
+
+// const first = document.querySelector(".first");
+// const classValue = first.getAttribute("class");
+// console.log(classValue);
+
+// const link = document.getElementById("link");
+
+// const pants = link.nextElementSibling;
+// pants.setAttribute("class", "soccer");
+
+// className - can be overwritten by accident!
+// classList - to add class
+
+// let first = document.getElementById("first");
+// let second = document.getElementById("second");
+
+// const classValue = first.className;
+// const classValue2 = second.className;
+// console.log(classValue);
+// console.log(classValue2);
+
+// first.className = "colors text";
+
+// second.classList.add("bing"); // to add multiple classes, do ("", "", "")
+// second.classList.remove("bing");
